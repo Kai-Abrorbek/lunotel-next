f49e84c18041644b6ee7d1909e617a4c641f52dd
Kai-Abrorbek <abror0dev@gmail.com>
2025-12-05 00:19:23 +0900

    fix: modify PopularDestination all business logic

--- libs/components/homepage/PopularDestinations.tsx
+++ libs/components/homepage/PopularDestinations.tsx
@@ -6,19 +6,19 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { useRouter } from 'next/router';
-import { PropertyLocation, PropertyLocationKorean } from '../../enums/property.enum';
+import { PropertyLocation } from '../../enums/property.enum';
 
-const ITEMS = [
-	{ name: '서울', image: '/img/서울.jfif', value: PropertyLocation.SEOUL },
-	{ name: '부산', image: '/img/부산.jfif', value: PropertyLocation.BUSAN },
-	{ name: '강릉', image: '/img/강릉.jfif', value: PropertyLocation.GANGNEUNG },
-	{ name: '인천', image: '/img/인천.jfif', value: PropertyLocation.INCHEON },
-	{ name: '경주', image: '/img/경주.jfif', value: PropertyLocation.GYEONGJU },
-	{ name: '해운대', image: '/img/해운대.jfif', value: PropertyLocation.HEUNDE },
-	{ name: '가평1', image: '/img/가평.jfif', value: PropertyLocation.GAPYEONG },
-	{ name: '가평2', image: '/img/가평.jfif', value: PropertyLocation.GAPYEONG },
-	{ name: '가평3', image: '/img/가평.jfif', value: PropertyLocation.GAPYEONG },
-	{ name: '가평4', image: '/img/가평.jfif', value: PropertyLocation.GAPYEONG },
+const Locations = [
+	{ name: '제주도', image: `/img/${PropertyLocation.JEJU}.jfif`, value: PropertyLocation.GAPYEONG },
+	{ name: '서울', image: `/img/${PropertyLocation.SEOUL}.jfif`, value: PropertyLocation.SEOUL },
+	{ name: '부산', image: `/img/${PropertyLocation.BUSAN}.jfif`, value: PropertyLocation.BUSAN },
+	{ name: '강릉', image: `/img/${PropertyLocation.GANGNEUNG}.jfif`, value: PropertyLocation.GANGNEUNG },
+	{ name: '인천', image: `/img/${PropertyLocation.INCHEON}.jfif`, value: PropertyLocation.INCHEON },
+	{ name: '경주', image: `/img/${PropertyLocation.GYEONGJU}.jfif`, value: PropertyLocation.GYEONGJU },
+	{ name: '해운대', image: `/img/${PropertyLocation.HEUNDE}.jfif`, value: PropertyLocation.HEUNDE },
+	{ name: '가평', image: `/img/${PropertyLocation.GAPYEONG}.jfif`, value: PropertyLocation.GAPYEONG },
+	{ name: '여수', image: `/img/${PropertyLocation.YEOSU}.jfif`, value: PropertyLocation.GAPYEONG },
+	{ name: '속초', image: `/img/${PropertyLocation.SOKCHO}.jfif`, value: PropertyLocation.GAPYEONG },
 ];
 
 export default function PopularDestinations() {
@@ -46,7 +46,7 @@ export default function PopularDestinations() {
 			<Box className="dest-container">
 				<Typography className="dest-title">국내 인기 여행지</Typography>
 
-				{ITEMS.length !== 0 ? (
+				{Locations.length !== 0 ? (
 					<Box className="dest-wrapper">
 						<div className="dest-nav dest-prev">
 							<ArrowBackIosNewIcon />
@@ -62,7 +62,7 @@ export default function PopularDestinations() {
 								nextEl: '.dest-next',
 							}}
 						>
-							{ITEMS.map((item) => (
+							{Locations.map((item) => (
 								<SwiperSlide key={item.name}>
 									<Box className="dest-card" onClick={() => handleSelectLocation(item.value)}>
 										<img src={item.image} className="dest-img" />
